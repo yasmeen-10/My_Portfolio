33c7e0d275f6b2632f90cac9d8b391abc61c8726
Yasmeen <90993453+yasmeen-10@users.noreply.github.com>
2023-09-30 16:59:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// resume link
+var resumeLink = document.getElementsByClassName("resume");
+resumeLink[0].addEventListener("click", function(){
+    window.open("https://drive.google.com/file/d/1TsGCEIrJaP8gWBipPPTZKTEcfLAk00eK/view","_blank");
+});
+
+
+// add smooth scroll on click navigation menu items
+
+var navMenuAncherTag = document.querySelectorAll('.nav-menu a');
+var interval;
+
+
+for(var i=0;i<navMenuAncherTag.length;i++){
+ navMenuAncherTag[i].addEventListener('click',function(event){
+     event.preventDefault(); // remove default action of ancher tag
+     var targetSectionId = this.textContent.trim().toLowerCase();
+     var targetSection = document.getElementById(targetSectionId);
+     console.log(targetSection);
+    // interval = setInterval(scrollVertically,20, targetSection);
+
+    interval = setInterval(function(){
+        scrollVertically(targetSection);
+    },20);
+
+ });
+}
+
+function scrollVertically(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if(targetSectionCoordinates.top <= 0){
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0,20);
+}
